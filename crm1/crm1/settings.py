085edaf034adb23feae57e23297b8ac7482671a1
Isaiah <isaiahlekay2001@gmail.com>
2021-07-08 17:42:39 +0200

    refractored

--- crm1/crm1/settings.py
+++ crm1/crm1/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e$@q6(ay_g#n-0ibn9#tpj#mm^2=18lvo=zp*&u%x8o=gpq$fg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
